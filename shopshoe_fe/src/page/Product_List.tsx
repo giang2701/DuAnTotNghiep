ddbf30d1a1d915a94e41b2531c5986b0965f71f9
Chuc1911 <151709710+Chuc1911@users.noreply.github.com>
2024-10-03 09:50:20 +0700

    xong hiển thị sp

--- shopshoe_fe/src/page/Product_List.tsx
+++ shopshoe_fe/src/page/Product_List.tsx
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ProductContext, ProductContextType } from '../context/ProductContext'
+import TymButton from '../component/Btn__tym';
+import { Link } from 'react-router-dom';
 
 export default function Product_List() {
-  const { state } = useContext(ProductContext) as ProductContextType,
+  const { state } = useContext(ProductContext) as ProductContextType
   const [gender, setGender] = useState('None');
-  const [priceRance, setPriceRange] = useState('None');
   const [priceRange, setPriceRange] = useState([0, 10000000]);
   const [isGenderOpen, setIsGenderOpen] = useState(false);
   const [isPriceRangeOpen, setIsPriceRangeOpen] = useState(false);
@@ -17,6 +18,19 @@ export default function Product_List() {
       setFilteredProducts(state.products);
     }, [state.products]
   )
+  const updateGender = (e) => {
+    setGender(e.target.value);
+  };
+
+  const updatePriceRange = (e, type) => {
+    const value = parseInt(e.target.value);
+    if (type === 'min') {
+      setPriceRange([value, priceRange[1]]);
+    } else {
+      setPriceRange([priceRange[0], value]);
+    }
+  };
+
 
   const applyFilters = () => {
     const filtered = state.products.filter((product) => {
@@ -25,6 +39,15 @@ export default function Product_List() {
         return matchesGender && matchesPrice;
     });
     setFilteredProducts(filtered);
+};
+ 
+  const formatCurrency = (price: number): string => {
+    return new Intl.NumberFormat("vi-VN", {
+        style: "currency",
+        currency: "VND",
+    })
+        .format(price)
+        .replace("₫", "đ");
 };
 
   return (
@@ -43,89 +66,139 @@ export default function Product_List() {
         <aside className="shop-sidebar">
           <div className="shop-sidebar__header">Bộ Lọc</div>
           <ul className="shop-sidebar__filters">
-                        <li
-                            className="shop-sidebar__filter"
-                            onClick={() => setIsGenderOpen(prev => !prev)}
-                        >
-                            Giới Tính
-                        </li>
-                        {isGenderOpen && (
-                            <div className="shop-sidebar__sub-list">
-                                <label className="shop-sidebar__sub-item">
-                                    <input
-                                        type="radio"
-                                        name="gender"
-                                        value="Nam"
-                                        checked={gender === 'Nam'}
-                                        onChange={updateGender}
-                                    />
-                                    Nam
-                                </label>
-                                <label className="shop-sidebar__sub-item">
-                                    <input
-                                        type="radio"
-                                        name="gender"
-                                        value="Nữ"
-                                        checked={gender === 'Nữ'}
-                                        onChange={updateGender}
-                                    />
-                                    Nữ
-                                </label>
-                                <label className="shop-sidebar__sub-item">
-                                    <input
-                                        type="radio"
-                                        name="gender"
-                                        value="None"
-                                        checked={gender === 'None'}
-                                        onChange={updateGender}
-                                    />
-                                    Không xác định
-                                </label>
-                            </div>
-                        )}
+            <li
+              className="shop-sidebar__filter"
+              onClick={() => setIsGenderOpen(prev => !prev)}
+            >
+              Giới Tính
+            </li>
+            {isGenderOpen && (
+              <div className="shop-sidebar__sub-list">
+                <label className="shop-sidebar__sub-item">
+                  <input
+                    type="radio"
+                    name="gender"
+                    value="Nam"
+                    checked={gender === 'Nam'}
+                    onChange={updateGender}
+                  />
+                  Nam
+                </label>
+                <label className="shop-sidebar__sub-item">
+                  <input
+                    type="radio"
+                    name="gender"
+                    value="Nữ"
+                    checked={gender === 'Nữ'}
+                    onChange={updateGender}
+                  />
+                  Nữ
+                </label>
+                <label className="shop-sidebar__sub-item">
+                  <input
+                    type="radio"
+                    name="gender"
+                    value="None"
+                    checked={gender === 'None'}
+                    onChange={updateGender}
+                  />
+                  Không xác định
+                </label>
+              </div>
+            )}
 
-                        <li
-                            className="shop-sidebar__filter"
-                            onClick={() => setIsPriceRangeOpen(prev => !prev)}
-                        >
-                            Mức giá
-                        </li>
-                        {isPriceRangeOpen && (
-                            <div className="shop-sidebar__range-slider">
-                                <label htmlFor="price-range">Mức giá</label>
-                                <input
-                                    type="range"
-                                    min="0"
-                                    max="10000000"
-                                    value={priceRange[0]}
-                                    onChange={(e) => updatePriceRange(e, 'min')}
-                                    className="slider-input"
-                                    id="price-range"
-                                />
-                                <span>{priceRange[0].toLocaleString()} đ</span>
-                            </div>
-                        )}
-                    </ul>
-                    <div className="shop-sidebar__buttons">
-                        <button
-                            className="shop-sidebar__reset-button"
-                            onClick={() => {
-                                setGender('None');
-                                setPriceRange([0, 10000000]);
-                                setFilteredProducts(state.products);
-                            }}
-                        >
-                            Bỏ lọc
-                        </button>
-                        <button
-                            className="shop-sidebar__filter-button"
-                            onClick={applyFilters}
-                        >
-                            Lọc
-                        </button>
-                    </div>
+            <li
+              className="shop-sidebar__filter"
+              onClick={() => setIsPriceRangeOpen(prev => !prev)}
+            >
+              Mức giá
+            </li>
+            {isPriceRangeOpen && (
+              <div className="shop-sidebar__range-slider">
+                <label htmlFor="price-range">Mức giá</label>
+                <input
+                  type="range"
+                  min="0"
+                  max="10000000"
+                  value={priceRange[0]}
+                  onChange={(e) => updatePriceRange(e, 'min')}
+                  className="slider-input"
+                  id="price-range"
+                />
+                <span>{priceRange[0].toLocaleString()} đ</span>
+              </div>
+            )}
+          </ul>
+          <div className="shop-sidebar__buttons">
+            <button
+              className="shop-sidebar__reset-button"
+              onClick={() => {
+                setGender('None');
+                setPriceRange([0, 10000000]);
+                setFilteredProducts(state.products);
+              }}
+            >
+              Bỏ lọc
+            </button>
+            <button
+              className="shop-sidebar__filter-button"
+              onClick={applyFilters}
+            >
+              Lọc
+            </button>
+          </div>
         </aside>
       </div>
+      <main className="shop-main-content">
+        <section className="shop-banner">
+          <img src="../../public/images/Banner__1.jpg" alt="Banner" className="shop-banner__image" />
+        </section>
+
+        <section className="shop-product-grid">
+          <div className="container">
+            <div className="Container__product gallery">
+              {filteredProducts.length > 0 ? (
+                filteredProducts.map((item) => (
+                  <div className="product gallery" key={item._id}>
+                    <div className="product-img">
+                      <img src={`${item.images}`} alt="" className="img-fluid" />
+                    </div>
+                    <div className="product-name">
+                      <Link
+                        to={`/detail/${item._id}`}
+                        style={{
+                          color: "#57585a",
+                          fontSize: "16px",
+                          lineHeight: "18px",
+                          fontWeight: "600",
+                          marginBottom: "10px",
+                          display: "block",
+                          textTransform: "capitalize",
+                          textDecoration: "none",
+                          margin: "20px 0px",
+                          lineHeight: "2.2rem",
+                          display: "-webkit-box",
+                          WebkitBoxOrient: "vertical",
+                          WebkitLineClamp: "1",
+                          overflow: "hidden",
+                        }}
+                      >
+                        {item.title}
+                      </Link>
+                      <p>{formatCurrency(item.price)}</p>
+                    </div>
+                    <div className="btn__tym">
+                      <TymButton />
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <p>Không tìm thấy sản phẩm nào phù hợp.</p>
+              )}
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   )
 }
