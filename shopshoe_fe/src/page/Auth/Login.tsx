d678e21898b6c11847ef702283782d8b3e8c3338
nguyen truong <truongnv7145@gmail.com>
2024-10-07 12:26:35 +0700

    features/login/register

--- shopshoe_fe/src/page/Auth/Login.tsx
+++ shopshoe_fe/src/page/Auth/Login.tsx
@@ -1,73 +1,93 @@
 import { joiResolver } from "@hookform/resolvers/joi";
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import instance from "../../api";
 import { useAuth } from "../../context/AuthContext";
 import { User } from "../../interface/User";
 import { LoginSchema } from "../../validate/AuthFormSchema";
 
 const Login = () => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<User>({
-        resolver: joiResolver(LoginSchema),
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<User>({
+    resolver: joiResolver(LoginSchema),
+  });
+  const nav = useNavigate();
+  const { login: contextLogin } = useAuth();
+  const onSubmitLogin = async (data: User) => {
+    const res = await instance.post(`/auth/login`, {
+      email: data.email,
+      password: data.password,
     });
-    const nav = useNavigate();
-    const { login: contextLogin } = useAuth();
-    const onSubmitLogin = async (data: User) => {
-        const res = await instance.post(`/auth/login`, {
-            email: data.email,
-            password: data.password,
-        });
-        contextLogin(res.data.accessToken, res.data.user);
-        // console.log(data);
-        nav("/");
-    };
-    return (
-        <>
-            <h1>Login</h1>
+    contextLogin(res.data.accessToken, res.data.user);
+    // console.log(data);
+    nav("/");
+  };
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-6 col-lg-6 col-md-12 col-ms-12 box__login">
+            <p>Bạn đã có tài khoản Zokong</p>
+            <p>
+              Nếu bạn đã có tài khoản, hãy đăng nhập để tích lũy điểm thành viên
+              và nhận được những ưu đãi tốt hơn!
+            </p>
             <form action="" onSubmit={handleSubmit(onSubmitLogin)}>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">
-                        Email
-                    </label>
-                    <input
-                        type="email"
-                        className="form-control"
-                        {...register("email", {
-                            required: "Email is required",
-                        })} // <-- Thêm yêu cầu cho `email`
-                    />
-                    {errors.email && (
-                        <span className="text-danger">
-                            {errors.email.message}
-                        </span>
-                    )}
-                </div>
+              <div className="mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  {...register("email", {
+                    required: "Email is required",
+                  })} // <-- Thêm yêu cầu cho `email`
+                  placeholder="Email"
+                />
+                {errors.email && (
+                  <span className="text-danger">{errors.email.message}</span>
+                )}
+              </div>
 
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">
-                        Password
-                    </label>
-                    <input
-                        type="password"
-                        className="form-control"
-                        {...register("password", {
-                            required: "Password is required",
-                        })} // <-- Thêm yêu cầu cho `password`
-                    />
-                    {errors.password && (
-                        <span className="text-danger">
-                            {errors.password.message}
-                        </span>
-                    )}
-                </div>
-                <button className="btn btn-success">Login</button>
+              <div className="mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  {...register("password", {
+                    required: "Password is required",
+                  })} // <-- Thêm yêu cầu cho `password`
+                  placeholder="Password"
+                />
+                {errors.password && (
+                  <span className="text-danger">{errors.password.message}</span>
+                )}
+              </div>
+              <button className="btn btn-success">ĐĂNG NHẬP</button>
+              <Link to="" className="btn btn-danger btn_link_register__mobile">
+                ĐĂNG KÝ
+              </Link>
             </form>
-        </>
-    );
+          </div>
+          <div className="col-xl-6 col-lg-6 col-md-12 col-ms-12 box__link__register ">
+            <p>Khách hàng mới của Zokong</p>
+            <p>
+              Nếu bạn chưa có tài khoản trên Zokong, hãy sử dụng tùy chọn này để
+              truy cập biểu mẫu đăng ký.
+            </p>
+            <p>
+              Bằng cách cung cấp cho Zokong thông tin chi tiết của bạn, quá
+              trình mua hàng trên Zokong sẽ là một trải nghiệm thú vị và nhanh
+              chóng hơn!
+            </p>
+            <Link to="" className="btn btn-danger btn_link_register">
+              ĐĂNG KÝ
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- shopshoe_fe/src/page/Auth/Register.tsx
+++ shopshoe_fe/src/page/Auth/Register.tsx
@@ -8,101 +8,103 @@ import { useNavigate } from "react-router-dom";
 type Props = {};
 
 const Register = (props: Props) => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<User>({
-        resolver: joiResolver(RegisterSchema),
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<User>({
+    resolver: joiResolver(RegisterSchema),
+  });
+  const nav = useNavigate();
+  const onSubmitRegister = async (data: User) => {
+    const res = await instance.post(`/auth/register`, {
+      username: data.username, // <-- Thêm `username` vào dữ liệu gửi đi
+      email: data.email,
+      password: data.password,
     });
-    const nav = useNavigate();
-    const onSubmitRegister = async (data: User) => {
-        const res = await instance.post(`/auth/register`, {
-            username: data.username, // <-- Thêm `username` vào dữ liệu gửi đi
-            email: data.email,
-            password: data.password,
-        });
-        alert(res.data.message);
-        nav("/login");
-        console.log(data);
-    };
-    return (
-        <>
-            <h1>Register</h1>
-            <form action="" onSubmit={handleSubmit(onSubmitRegister)}>
-                <div className="mb-3">
-                    <label htmlFor="username" className="form-label">
-                        Username
-                    </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        {...register("username", {
-                            required: "Username is required",
-                        })} // <-- Đăng ký `username` và thêm yêu cầu
-                    />
-                    {errors.username && (
-                        <span className="text-danger">
-                            {errors.username.message}
-                        </span>
-                    )}
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">
-                        Email
-                    </label>
-                    <input
-                        type="email"
-                        className="form-control"
-                        {...register("email", {
-                            required: "Email is required",
-                        })} // <-- Thêm yêu cầu cho `email`
-                    />
-                    {errors.email && (
-                        <span className="text-danger">
-                            {errors.email.message}
-                        </span>
-                    )}
-                </div>
+    alert(res.data.message);
+    nav("/login");
+    console.log(data);
+  };
+  return (
+    <>
+      <div className="container">
+        <div className="register">
+          <p>ĐĂNG KÝ</p>
+          <form action="" onSubmit={handleSubmit(onSubmitRegister)}>
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">
+                Username
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Username..."
+                {...register("username", {
+                  required: "Username is required",
+                })} // <-- Đăng ký `username` và thêm yêu cầu
+              />
+              {errors.username && (
+                <span className="text-danger">{errors.username.message}</span>
+              )}
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email..."
+                {...register("email", {
+                  required: "Email is required",
+                })} // <-- Thêm yêu cầu cho `email`
+              />
+              {errors.email && (
+                <span className="text-danger">{errors.email.message}</span>
+              )}
+            </div>
 
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">
-                        Password
-                    </label>
-                    <input
-                        type="password"
-                        className="form-control"
-                        {...register("password", {
-                            required: "Password is required",
-                        })} // <-- Thêm yêu cầu cho `password`
-                    />
-                    {errors.password && (
-                        <span className="text-danger">
-                            {errors.password.message}
-                        </span>
-                    )}
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="confirmPass" className="form-label">
-                        Confirm Password
-                    </label>
-                    <input
-                        type="password"
-                        className="form-control"
-                        {...register("confirmPass", {
-                            required: "Confirm Password is required",
-                        })} // <-- Thêm yêu cầu cho `confirmPass`
-                    />
-                    {errors.confirmPass && (
-                        <span className="text-danger">
-                            {errors.confirmPass.message}
-                        </span>
-                    )}
-                </div>
-                <button className="btn btn-success">Register</button>
-            </form>
-        </>
-    );
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password..."
+                {...register("password", {
+                  required: "Password is required",
+                })} // <-- Thêm yêu cầu cho `password`
+              />
+              {errors.password && (
+                <span className="text-danger">{errors.password.message}</span>
+              )}
+            </div>
+            <div className="mb-3">
+              <label htmlFor="confirmPass" className="form-label">
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Confirm Password..."
+                {...register("confirmPass", {
+                  required: "Confirm Password is required",
+                })} // <-- Thêm yêu cầu cho `confirmPass`
+              />
+              {errors.confirmPass && (
+                <span className="text-danger">
+                  {errors.confirmPass.message}
+                </span>
+              )}
+            </div>
+            <button className="btn button-register">ĐĂNG KÝ</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Register;
