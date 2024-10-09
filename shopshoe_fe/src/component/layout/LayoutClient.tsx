9a655d4db15608a6e8c032838944d2bc5bd67c71
giang <vgiang2701@gmail.com>
2024-10-09 23:11:16 +0700

    chỉnh sửa header/banner

--- shopshoe_fe/src/component/layout/LayoutClient.tsx
+++ shopshoe_fe/src/component/layout/LayoutClient.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
-import Header from "../Header";
+// import Header from "../Header";
 import Footer from "../Footer";
+import Header2 from "../Header2";
 
 const LayoutClient = () => {
     return (
         <div>
-            <Header />
+            <Header2 />
             <Outlet />
             <Footer />
         </div>
