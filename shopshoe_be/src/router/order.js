3becd34e9e5a5f34e00cd75e175e84e95c6df359
giang <vgiang2701@gmail.com>
2024-11-29 09:26:53 +0700

    fix lỗi order, thêm phần hồi stock thì huyr đơn hàng

--- shopshoe_be/src/router/order.js
+++ shopshoe_be/src/router/order.js
@@ -1,15 +1,16 @@
 import express from "express";
 import {
-  checkAndUpdatePaymentStatus,
-  createDetail,
-  createOrder,
-  createOrderAndPayment,
-  deleteOrder,
-  getAllOrders,
-  getOrderDetail,
-  getOrderDetailById,
-  momoWebhook,
-  updateOrderStatus,
+    checkAndUpdatePaymentStatus,
+    createDetail,
+    createOrder,
+    createOrderAndPayment,
+    deleteOrder,
+    getAllOrders,
+    getOrderDetail,
+    getOrderDetailById,
+    momoWebhook,
+    updateOrderStatus,
+    updateStatusCancel,
 } from "../controller/Orders.js";
 const RouterOrder = express.Router();
 RouterOrder.post("/", createOrder);
@@ -18,6 +19,7 @@ RouterOrder.get("/", getAllOrders);
 RouterOrder.get("/:id", getOrderDetail);
 RouterOrder.get("/user/:id", getOrderDetailById);
 RouterOrder.put("/status/:id", updateOrderStatus);
+RouterOrder.put("/statusCancel/:id", updateStatusCancel);
 RouterOrder.delete("/:id", deleteOrder);
 RouterOrder.post("/payWithMoMo", createOrderAndPayment);
 RouterOrder.post("/query", checkAndUpdatePaymentStatus);
